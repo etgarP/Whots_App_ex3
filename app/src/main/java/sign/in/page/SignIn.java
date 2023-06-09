874f75543fb2720d3aaacc74712ba0875fa2728a
etgarP <etgar12340@gmail.com>
2023-06-09 17:20:39 +0300

    added ability to go between them

--- app/src/main/java/sign/in/page/SignIn.java
+++ app/src/main/java/sign/in/page/SignIn.java
@@ -2,15 +2,26 @@ package sign.in.page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.whotsapp.R;
+import com.example.whotsapp.databinding.SignInBinding;
+
+import sign.up.page.Register;
+
 
 public class SignIn extends AppCompatActivity {
-
+    private SignInBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in);
+        binding = SignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.toRegister.setOnClickListener(view -> {
+            Intent intent = new Intent(SignIn.this, Register.class);
+            startActivity(intent);
+        });
     }
 }
