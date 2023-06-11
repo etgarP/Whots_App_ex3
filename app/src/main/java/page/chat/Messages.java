1133d20563693e830dd1b758a50befadac234f17
LiorNakar <nakarlior@gmail.com>
2023-06-12 02:09:20 +0300

    trying to put messages to start from bottom

--- app/src/main/java/page/chat/Messages.java
+++ app/src/main/java/page/chat/Messages.java
@@ -23,72 +23,27 @@ public class Messages extends AppCompatActivity {
         setContentView(R.layout.activity_messages);
 
         RecyclerView lstMessages = findViewById(R.id.lstMessages);
+
         final MessagesListAdapter adapter = new MessagesListAdapter(this);
         lstMessages.setAdapter(adapter);
-        lstMessages.setLayoutManager(new LinearLayoutManager(this));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        lstMessages.setLayoutManager(layoutManager);
 
         List<Message> messages = new ArrayList<>();
         User user = new User("Hemi", "hemi hemi 34", "1");
         messages.add(new Message("2",user,"1"));
+        messages.add(new Message("2",user,"2"));
+        messages.add(new Message("2",user,"3"));
         messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
-        messages.add(new Message("2",user,"4"));
+        messages.add(new Message("2",user,"5"));
+        messages.add(new Message("2",user,"6"));
+        messages.add(new Message("2",user,"7"));
+        messages.add(new Message("2",user,"8"));
         messages.add(new Message("2",user,"9"));
-        messages.add(new Message("2",user,"9"));
-        messages.add(new Message("2",user,"9"));
-        messages.add(new Message("2",user,"9"));
-        messages.add(new Message("2",user,"9"));
-        messages.add(new Message("2",user,"9"));
+
 
 
 
