b10eef040582c48b01cf2a2657dacfc67ab7f0dc
etgarP <etgar12340@gmail.com>
2023-06-11 13:05:38 +0300

    more chat design

---
+++ app/src/main/java/page/chat/messages.java
@@ -0,0 +1,16 @@
+package page.chat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.whotsapp.R;
+
+public class messages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+    }
+}
